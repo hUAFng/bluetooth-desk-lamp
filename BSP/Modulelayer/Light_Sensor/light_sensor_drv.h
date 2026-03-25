829b3a8b025830f8e6e51db1d0adc4ae18908408
hUAFeng <3252146438@qq.com>
2026-03-25 15:01:59 +0800

    完成了光照传感器—drv的编写

--- BSP/Modulelayer/Light_Sensor/light_sensor_drv.h
+++ BSP/Modulelayer/Light_Sensor/light_sensor_drv.h
@@ -17,8 +17,8 @@ extern I2C_HandleTypeDef hi2c1;
 #define LS_POWON 0x01   //上电
 #define LS_POWOFF 0x00  //断电-进入低功耗模式
 #define LS_RESET 0x02   //重置
-#define LS_HRES_MODE1 0x10  //连续高分辨率模式1 - 1lx分辨率,测量时间120ms,默认常用模式
-#define LS_HRES_MODE2 0x11  //连续高分辨率模式2 - 0.5lx分辨率,测量时间120ms,适合弱光环境
+#define LS_HRES_MODE1 0x10  //连续高分辨率模式1 - 1lx分辨率,测量时间120ms,默认常用模式 量程大
+#define LS_HRES_MODE2 0x11  //连续高分辨率模式2 - 0.5lx分辨率,测量时间120ms,适合弱光环境 
 #define LS_LRES_MODE 0x13   //连续低分辨率模式 - 4lx分辨率,测量时间16ms,适合快速采样
 #define LS_SINGLE_MEAS_MODE 0x20  //单次测量模式，测量一次后自动断电,适合低功耗场景
 
@@ -32,5 +32,14 @@ typedef enum
 }LS_MODE;
         
 
-#endif
 
+/* -------------------------------------function ------------------------------------- */
+void ls_Init(void);
+HAL_StatusTypeDef ls_SetMode(LS_MODE mode);
+HAL_StatusTypeDef ls_MeasureLight(LS_MODE mode,float* lux);
+void ls_ChangeModeByLus(float* lux,LS_MODE cnt_mode);
+void ls_Poweroff(void);
+
+
+
+#endif
